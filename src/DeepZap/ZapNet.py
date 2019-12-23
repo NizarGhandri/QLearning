809165cd16b3037550c334cdb4253aaff13cbe06
Nizar G <nizar.ghandri115@gmail.com>
2019-12-23 14:20:51 +0100

    coded nn

--- src/DeepZap/ZapNet.py
+++ src/DeepZap/ZapNet.py
@@ -12,11 +12,12 @@ Created on Sat Dec 21 15:31:30 2019
 
 import numpy as np
 import ActivationFunctions 
-import Network
+from Network import Network
+import gym
 
 class ZapNet: 
     
-    def __init__(self, dimensions, beta=0.99, zapgain=0.999999999999999999999999999999999999999999999, epsilonzap=0.00000000000001):
+    def __init__(self, dimensions, environment, beta=0.99, zapgain=0.999999999999999999999999999999999999999999999, epsilonzap=0.00000000000001):
         """
         :param dimensions: list of dimensions of the neural net. (input, hidden layer, ... ,hidden layer, output)
         Example of one hidden layer with
@@ -34,30 +35,85 @@ class ZapNet:
         self.beta = beta
         self.zapgain = zapgain
         self.epsilonzap = epsilonzap
+        self.env = gym.make(environment)
+        self.n = 0
+        self.A = []
+        temp = len(dimensions)-1
+        for i in range(temp):
+            self.A.append([np.zeros((dimensions[i+1], dimensions[i+1])) for j in range(dimensions[i])])
     
     def nlayers (self):
         return self.neural_net.nlayers
         
     def action(self, s):
-        return np.argmax(self.neural_net.predict(s))
-        
+        return (np.argmax(self.neural_net.predict(s))-1/2)*2
+    
+    ###### for now only supports linear activation function    
     def update_layer(self, reward, a, a_, i):
         number_of_neurones = self.neural_net.dimensions[i]
         number_of_neurones_next_layer = self.neural_net.dimensions[i+1]
         for j in range(number_of_neurones):
-            swig = np.full(number_of_neurones_next_layer, self.neural_net.a[i, 0])
-            swig_f = np.full(number_of_neurones_next_layer, self.neural_net.a_[i, 0])
+            swig = np.full(number_of_neurones_next_layer, self.neural_net.a[i, j])
+            swig_f = np.full(number_of_neurones_next_layer, self.neural_net.a_[i, j])
             d_n_1 = reward + self.beta * self.neural_net.w[i,j]@swig_f - self.neural_net.w[i,j]@swig
             A_n_1 = swig@(self.beta*self.swig_f-swig)
-            self.A = (self.A + (A_n_1 - self.A)*self.zapgain) 
-            G = -np.linalg.pinv(A + self.epsilonzap * np.eye(number_of_neurones_next_layer))
+            self.A[i,j] = (self.A[i,j] + (A_n_1 - self.A[i,j])*self.zapgain) 
+            G = -np.linalg.pinv(self.A[i,j] + self.epsilonzap * np.eye(number_of_neurones_next_layer))
             self.neural_net.w[i,j] = self.neural_net.w[i,j] + self.alpha_n()*G@swig*d_n_1
         
-    def update(self, reward, a, a_):
-        for i in range(self.nlayers()-1, 0, -1):
+    def update(self, observation, observationN, action, reward):
+        _, a = self.neural_net.feedforward(observation)
+        _, a_ = self.neural_net.feedforward(observationN)
+        
+        for i in range(self.nlayers()-1, 1, -1):
             self.update_layer(reward, a, a_, i)
+        self.n += 1
             
-    
+    def fit(self, number_of_episodes, timestep_per_episode):
+         e = 0.5
+         for i_episode in range(number_of_episodes):
+            observation = self.env.reset()
+            for t in range(timestep_per_episode):
+                self.env.render()
+                if (np.random.uniform() < e):
+                    action = self.env.action_space.sample()
+                else:
+                    action = self.action(observation)
+                observationN, reward, done, info = self.env.step(action)
+                self.update(observation, observationN, action, reward)
+                observation = observationN
+                if done:
+                    print("Episode finished after {} timesteps".format(t+1))
+
+
+
+    def evaluate(self, number_of_episodes, timestep_per_episode):
+        rewards = []
+        self.n = 0
+        for i_episode in range(number_of_episodes):
+                r = 0
+                observation = self.env.reset()
+                for t in range(timestep_per_episode):
+                    self.env.render()
+                    action = self.action(observation)
+                    observationN, reward, done, info = self.env.step(action)
+                    self.update(observation, observationN, action, reward)
+                    observation = observationN
+                    r += reward
+                    if done:
+                        print("Episode finished after {} timesteps".format(t+1))
+                        break
+                rewards.append(r)
+        f = np.split(np.array(rewards), number_of_episodes//100)
+        plotted = []
+        for r in f: 
+            z = sum(r)/100
+            print(z)
+            plotted.append(z)
+        plt.plot(plotted)
+        plt.show()
+        self.env.close()
+            
         
 # =============================================================================
 #     def reward(x, y, loss, lmbd=0.1):
